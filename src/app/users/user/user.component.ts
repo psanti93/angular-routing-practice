247a17919e2256ddbdf2f6a5a4967aebe874efaf
Paul Santiago <paulsantiago282@gmail.com>
2023-09-07 17:59:34 -0400

    Adding parameters reactively through an observable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,10 +12,22 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // best practice to use when your component will never update with new parameters
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name'],
     };
+
+    // this will now update the object reactively with the values that are given for id and name
+    // best practice to use when your routed component is being reoloaded multiple times with new parameters
+
+    this.route.params
+      .subscribe(
+        (params: Params) =>{
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
