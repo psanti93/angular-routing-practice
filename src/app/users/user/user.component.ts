bb5cfe1ec3d776e29c0b2b1015154bb45807f6cc
Paul Santiago <paulsantiago282@gmail.com>
2023-09-07 18:06:14 -0400

    Add note about destroying subscriptions

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,14 +22,19 @@ export class UserComponent implements OnInit {
 
     // this will now update the object reactively with the values that are given for id and name
     // best practice to use when your routed component is being reoloaded multiple times with new parameters
-
-    this.route.params
+    this.paramSubscription = this.route.params
       .subscribe(
         (params: Params) =>{
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+
+  // In theory subscriptions will destroy your subscriptions after it's done being used, however it doesn't hurt do unsbscribe using the ngOnDestroy lifecycle
+
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
   }
 
 }
